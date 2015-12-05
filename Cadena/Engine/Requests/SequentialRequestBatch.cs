d4af439e80fe53edf9cb5fa9b7b584c29c2512db
Karno <karnoroid@gmail.com>
2015-12-05 20:56:58 +0900

    ＼(#`・△・)/ < Improve timeout implementation in SequentialRequestBatch!

--- Cadena/Engine/Requests/SequentialRequestBatch.cs
+++ Cadena/Engine/Requests/SequentialRequestBatch.cs
@@ -23,12 +23,13 @@ namespace Cadena.Engine.Requests
         /// If this batch was completed its execution already, will throws exception.
         /// </summary>
         /// <param name="request">queued request</param>
+        /// <param name="token">cancellation token</param>
         /// <exception cref="InvalidOperationException">execution of this batch was already completed.</exception>
         /// <returns>Task that will complete after request will be executed.</returns>
         [NotNull]
-        public Task Enqueue(IRequest request)
+        public Task Enqueue(IRequest request, CancellationToken token = new CancellationToken())
         {
-            var task = TryEnqueue(request);
+            var task = TryEnqueue(request, token);
             if (task == null)
             {
                 throw new InvalidOperationException("The execution of this batch had been completed already.");
@@ -40,9 +41,10 @@ namespace Cadena.Engine.Requests
         /// Enqueue new task.
         /// </summary>
         /// <param name="request">queued request</param>
+        /// <param name="token">cancellation token</param>
         /// <returns>Task that will complete after request will be executed.</returns>
         [CanBeNull]
-        public Task TryEnqueue(IRequest request)
+        public Task TryEnqueue(IRequest request, CancellationToken token = new CancellationToken())
         {
             BatchedRequest bt;
             lock (_requestQueue)
@@ -51,22 +53,64 @@ namespace Cadena.Engine.Requests
                 {
                     return null;
                 }
-                _requestQueue.Enqueue(bt = new BatchedRequest(request));
+                _requestQueue.Enqueue(bt = new BatchedRequest(request, token));
             }
             return bt.ResultTask;
         }
+
+        /// <summary>
+        /// Enqueue new task.
+        /// If this batch was completed its execution already, will throws exception.
+        /// </summary>
+        /// <param name="request">queued request</param>
+        /// <param name="timeout">timeout which started from starting process of this request</param>
+        /// <exception cref="InvalidOperationException">execution of this batch was already completed.</exception>
+        /// <returns>Task that will complete after request will be executed.</returns>
+        [NotNull]
+        public Task Enqueue(IRequest request, TimeSpan timeout)
+        {
+            var task = TryEnqueue(request, timeout);
+            if (task == null)
+            {
+                throw new InvalidOperationException("The execution of this batch had been completed already.");
+            }
+            return task;
+        }
+
+        /// <summary>
+        /// Enqueue new task.
+        /// </summary>
+        /// <param name="request">queued request</param>
+        /// <param name="timeout">timeout which started from starting process of this request</param>
+        /// <returns>Task that will complete after request will be executed.</returns>
+        [CanBeNull]
+        public Task TryEnqueue(IRequest request, TimeSpan timeout)
+        {
+            BatchedRequest bt;
+            lock (_requestQueue)
+            {
+                if (_executionCompleted)
+                {
+                    return null;
+                }
+                _requestQueue.Enqueue(bt = new BatchedRequest(request, timeout));
+            }
+            return bt.ResultTask;
+        }
+
 
         /// <summary>
         /// Enqueue new *typed* task.
         /// If this batch was completed its execution already, will throws exception.
         /// </summary>
         /// <param name="request">queued request</param>
+        /// <param name="token">cancellation token</param>
         /// <exception cref="InvalidOperationException">execution of this batch was already completed.</exception>
         /// <returns>Task that will complete after request will be executed.</returns>
         [NotNull]
-        public Task<T> Enqueue<T>(IRequest<T> request)
+        public Task<T> Enqueue<T>(IRequest<T> request, CancellationToken token = new CancellationToken())
         {
-            var task = TryEnqueue(request);
+            var task = TryEnqueue(request, token);
             if (task == null)
             {
                 throw new InvalidOperationException("The execution of this batch had been completed already.");
@@ -78,9 +122,10 @@ namespace Cadena.Engine.Requests
         /// Enqueue new *typed* task.
         /// </summary>
         /// <param name="request">queued request</param>
+        /// <param name="token">cancellation token</param>
         /// <returns>Task that will complete after request will be executed.</returns>
         [CanBeNull]
-        public Task<T> TryEnqueue<T>(IRequest<T> request)
+        public Task<T> TryEnqueue<T>(IRequest<T> request, CancellationToken token = new CancellationToken())
         {
             BatchedRequest<T> bt;
             lock (_requestQueue)
@@ -89,10 +134,51 @@ namespace Cadena.Engine.Requests
                 {
                     return null;
                 }
-                _requestQueue.Enqueue(bt = new BatchedRequest<T>(request));
+                _requestQueue.Enqueue(bt = new BatchedRequest<T>(request, token));
             }
             return bt.ResultTask;
         }
+
+        /// <summary>
+        /// Enqueue new *typed* task.
+        /// If this batch was completed its execution already, will throws exception.
+        /// </summary>
+        /// <param name="request">queued request</param>
+        /// <param name="timeout">timeout which started from starting process of this request</param>
+        /// <exception cref="InvalidOperationException">execution of this batch was already completed.</exception>
+        /// <returns>Task that will complete after request will be executed.</returns>
+        [NotNull]
+        public Task<T> Enqueue<T>(IRequest<T> request, TimeSpan timeout)
+        {
+            var task = TryEnqueue(request, timeout);
+            if (task == null)
+            {
+                throw new InvalidOperationException("The execution of this batch had been completed already.");
+            }
+            return task;
+        }
+
+        /// <summary>
+        /// Enqueue new *typed* task.
+        /// </summary>
+        /// <param name="request">queued request</param>
+        /// <param name="timeout">timeout which started from starting process of this request</param>
+        /// <returns>Task that will complete after request will be executed.</returns>
+        [CanBeNull]
+        public Task<T> TryEnqueue<T>(IRequest<T> request, TimeSpan timeout)
+        {
+            BatchedRequest<T> bt;
+            lock (_requestQueue)
+            {
+                if (_executionCompleted)
+                {
+                    return null;
+                }
+                _requestQueue.Enqueue(bt = new BatchedRequest<T>(request, timeout));
+            }
+            return bt.ResultTask;
+        }
+
 
         public IEnumerator<IRequest> GetEnumerator()
         {
@@ -152,11 +238,25 @@ namespace Cadena.Engine.Requests
         private sealed class BatchedRequest : IBatchedRequest
         {
             private readonly IRequest _request;
+            private readonly TimeSpan _timeout;
+            private readonly CancellationToken _token;
             private readonly TaskCompletionSource<object> _completionSource;
 
-            public BatchedRequest(IRequest request)
+            public BatchedRequest(IRequest request, CancellationToken token)
+                : this(request, token, Timeout.InfiniteTimeSpan)
+            {
+            }
+
+            public BatchedRequest(IRequest request, TimeSpan timeout)
+                : this(request, CancellationToken.None, timeout)
+            {
+            }
+
+            private BatchedRequest(IRequest request, CancellationToken token, TimeSpan timeout)
             {
                 _request = request;
+                _timeout = timeout;
+                _token = token;
                 _completionSource = new TaskCompletionSource<object>();
             }
 
@@ -165,14 +265,13 @@ namespace Cadena.Engine.Requests
 
             public async Task Send(CancellationToken token)
             {
+                var localTokenSource = CancellationTokenSource.CreateLinkedTokenSource(token, _token);
+                localTokenSource.CancelAfter(_timeout);
+                var localToken = localTokenSource.Token;
                 try
                 {
-                    await _request.Send(token);
+                    await _request.Send(localToken);
                     _completionSource.SetResult(null);
-                }
-                catch (TaskCanceledException)
-                {
-                    _completionSource.SetCanceled();
                 }
                 catch (Exception ex)
                 {
@@ -188,11 +287,25 @@ namespace Cadena.Engine.Requests
         private sealed class BatchedRequest<T> : IBatchedRequest
         {
             private readonly IRequest<T> _request;
+            private readonly TimeSpan _timeout;
+            private readonly CancellationToken _token;
             private readonly TaskCompletionSource<T> _completionSource;
 
-            public BatchedRequest(IRequest<T> request)
+            public BatchedRequest(IRequest<T> request, CancellationToken token)
+                : this(request, token, Timeout.InfiniteTimeSpan)
+            {
+            }
+
+            public BatchedRequest(IRequest<T> request, TimeSpan timeout)
+                : this(request, CancellationToken.None, timeout)
+            {
+            }
+
+            private BatchedRequest(IRequest<T> request, CancellationToken token, TimeSpan timeout)
             {
                 _request = request;
+                _timeout = timeout;
+                _token = token;
                 _completionSource = new TaskCompletionSource<T>();
             }
 
@@ -201,14 +314,13 @@ namespace Cadena.Engine.Requests
 
             public async Task Send(CancellationToken token)
             {
+                var localTokenSource = CancellationTokenSource.CreateLinkedTokenSource(token, _token);
+                localTokenSource.CancelAfter(_timeout);
+                var localToken = localTokenSource.Token;
                 try
                 {
-                    var result = await _request.Send(token);
+                    var result = await _request.Send(localToken);
                     _completionSource.SetResult(result);
-                }
-                catch (TaskCanceledException)
-                {
-                    _completionSource.SetCanceled();
                 }
                 catch (Exception ex)
                 {
