956b419551703d882eae6680045a20652ccc66fc
Karno <karnoroid@gmail.com>
2015-12-23 18:12:39 +0900

    ＼(#`・△・)/ < Add initialization helper class!

---
+++ Cadena/CadenaInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using AsyncOAuth;
+
+namespace Cadena
+{
+    public static class CadenaInitializer
+    {
+        public static void Initialize()
+        {
+            // set ServicePointManager properties.
+            ServicePointManager.Expect100Continue = false;
+            ServicePointManager.DefaultConnectionLimit = Int32.MaxValue;
+            // set hash function
+            OAuthUtility.ComputeHash = (key, buffer) =>
+            {
+                using (var hmac = new HMACSHA1(key))
+                {
+                    return hmac.ComputeHash(buffer);
+                }
+            };
+        }
+    }
+}
